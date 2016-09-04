ea2486e832895f7c37cbb4d70449821034d3b1aa
Thomas Whitburn <twhitburn@wisc.edu>
2016-09-04 22:04:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RasberryPi-Internet-Radio
---
+++ radio.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while true; do
+
+printf "\nPress 1 to play, 2 to stop, or 3 to exit:\n"
+read -n1  -p "Selection: " input
+
+if [ "$input" == 1 ]; then 
+    echo ""
+     mpc play
+
+elif [ "$input" == 2 ]; then
+    echo ""
+    mpc stop
+
+elif [ "$input" == 3 ]; then 
+    echo ""
+    mpc stop
+    exit
+else 
+    printf "\nInvalid input!\n"
+fi 
+
+done
+
